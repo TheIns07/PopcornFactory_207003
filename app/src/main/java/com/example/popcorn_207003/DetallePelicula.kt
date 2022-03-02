ea94204d5a8ba4b417677c8fc0661b81a60ad974
Arturo Cervantes <jesus.cervantes207003@potros.itson.edu.mx>
2022-03-02 14:26:53 -0700

    Versiones corregidas con los colores y ajustes en las ventanas

--- app/src/main/java/com/example/popcorn_207003/DetallePelicula.kt
+++ app/src/main/java/com/example/popcorn_207003/DetallePelicula.kt
@@ -5,18 +5,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_detalle_pelicula.*
+import kotlinx.android.synthetic.main.movie.*
 
 
 class DetallePelicula : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.begginning_main)
+        setContentView(R.layout.activity_detalle_pelicula)
 
-        var btnMain: Button = findViewById(R.id.button_beg) as Button
+        var bundle = intent.extras
+
+        /**var btnMain: Button = findViewById(R.id.button_beg) as Button
 
         btnMain.setOnClickListener{
             var intent: Intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+        }
+        */
+        if(bundle != null){
+            iv_pelicula_imagen.setImageResource(bundle.getInt("header"))
+            tv_pelicula_nombre.setText(bundle.getString("nombre"))
+            iv_pelicula_sinopsis.setText(bundle.getString("sinopsis"))
         }
 
     }
--- app/src/main/java/com/example/popcorn_207003/MainActivity.kt
+++ app/src/main/java/com/example/popcorn_207003/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.begginning_main)
 
         cargarPeliculas()
         var listView: GridView = findViewById(R.id.gridview) as GridView
